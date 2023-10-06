3797f49cfa5e27637c68c8b80f7aa9b4983f1e21
Caique Moreira <caique_moreira_sz@hotmail.com>
2023-10-06 10:20:35 -0300

    feat: card click to album page

--- src/components/Cards/AlbumCard.jsx
+++ src/components/Cards/AlbumCard.jsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 export default function AlbumCard({ data }) {
+  const navigate = useNavigate();
+  function handleCardClick() {
+    navigate(`/album/${data.collectionId}`);
+  }
+
   return (
     <div
       id="card-container"
       className="first:ml-6 first:mt-6 w-1/6 border border-1 border-black flex flex-col items-center justify-normal rounded-md space-y-6 text-center hover:cursor-pointer hover:-translate-y-2 hover:bg-gray-100 delay-75"
+      onClick={handleCardClick}
     >
       <img
         className="w-full h-1/2 rounded-t-md"
